9dc76528f94a498c6e9b4409da26aa4a27d482cc
KarenGalicia <kjimenezg6@miumg.edu.gt>
2025-02-21 00:01:45 -0600

    fix: server endpoint

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let accessToken = '';
 
 async function getToken() {
     try {
-        const response = await fetch('https://tu-backend-spotify.onrender.com/get-token');
+        const response = await fetch('https://cola-spotify.onrender.com/get-token');
         const data = await response.json();
 
         if (response.ok) {
